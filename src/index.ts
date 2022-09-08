47750410e64e302e0b10237ca7dd57790943ea73
chocoscoding <chocoscoding@gmail.com>
2022-09-08 21:30:29 +0100

    .

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,12 @@ let rabbitconn = 4;
 const PORT: number | string = process.env.PORT || 9987;
 const app: Express = express();
 
-app.use(cors());
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/signup", SignupRouter);
